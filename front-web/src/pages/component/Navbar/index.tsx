b7f8b1529808b260f518b80933870e097391eb4b
Wellington Mazoni <wmazoni@protonmail.com>
2021-10-21 23:45:07 -0300

    Create Navbar

---
+++ front-web/src/pages/component/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+        <h4 className="nav-text">MovieFlix</h4>
+    </nav>
+  );
+};
+
+export default Navbar;
